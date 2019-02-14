424c0cca7bf2f3fbd6801ff96c6e844e19ea5ab3
Rafal Pijet <stronglopez@wp.pl>
2019-02-14 13:49:34 +0100

    Adding additional recommendation from 19.4 task

--- script-compiled.js
+++ script-compiled.js
@@ -9,6 +9,7 @@ console.log(first_word + " " + second_word);
 function say(first, second) {
     console.log(first + " " + second);
 }
+
 say(first_word, second_word);
 
 var show = function show(first, second) {
@@ -53,7 +54,6 @@ var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 average.apply(undefined, grades);
 
 console.log("Task_5 -------------------:");
-
 var data = [1, 4, 'Iwona', false, 'Nowak'];
 
 var firstname = data[2],
@@ -61,3 +61,97 @@ var firstname = data[2],
 
 
 console.log("Name: " + firstname + " " + lastname);
+
+console.log("Additional Task_1 --------:");
+
+function songDecoder(song) {
+    var djText = "WUB";
+    var textQuantity = "";
+    var count = 0;
+    var result = "";
+
+    for (var j = 0; j < song.length; j++) {
+
+        while (count == 0) {
+            count = Math.floor(Math.random() * 10);
+        }
+
+        for (var i = 0; i < count; i++) {
+            textQuantity += djText;
+        }
+        song = song.replace(" ", textQuantity);
+    }
+
+    count = Math.floor(Math.random() * 10);
+    textQuantity = "";
+
+    for (var _i = 0; _i < count; _i++) {
+        textQuantity += djText;
+    }
+
+    result = "" + textQuantity + song + textQuantity;
+
+    return result;
+}
+
+console.log(songDecoder("--wow-- --more-- --next--"));
+
+console.log("Additional Task_2 --------:");
+
+function shuffleIt(arr) {
+
+    var result = arr;
+
+    for (var _len2 = arguments.length, arrayTwoPosition = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+        arrayTwoPosition[_key2 - 1] = arguments[_key2];
+    }
+
+    arrayTwoPosition.forEach(function (array) {
+        var first = arr[array[1]];
+        var second = arr[array[0]];
+        result[array[0]] = first;
+        result[array[1]] = second;
+    });
+
+    return result;
+}
+
+var arrayIn = [1, 2, 3, 4, 5];
+var arrayChange_1 = [0, 1];
+var arrayChange_2 = [1, 2];
+var arrayChange_3 = [2, 3];
+var result = shuffleIt(arrayIn, arrayChange_1, arrayChange_2, arrayChange_3);
+console.log("Array before change --> [" + arrayIn + "], and after change --> [" + result + "]");
+
+console.log("Additional Task_3 --------:");
+
+var arrayNumbers_1 = [65, 44, 12, 4];
+var arrayNumbers_2 = [136, 82, 72, 15];
+
+function sum(array) {
+    return array.reduce(function (total, num) {
+        return total + num;
+    });
+}
+
+console.log("Sum numbers in array: [" + arrayNumbers_1 + "] equals: " + sum(arrayNumbers_1));
+console.log("Sum numbers in array: [" + arrayNumbers_2 + "] equals: " + sum(arrayNumbers_2));
+
+console.log("Additional Task_4 --------:");
+
+function sumNumbers(newArray) {
+    var result = 0;
+    newArray.forEach(function (element) {
+        result += element;
+    });
+    return result;
+}
+
+function spread(func, args) {
+    return func(args);
+}
+
+var exampleArray_1 = [1, 2, 3, 4, 5, 6, 7];
+var exampleArray_2 = [6, 7, 8, 9, 2, 5, 8];
+console.log("Effect: " + spread(sumNumbers, exampleArray_1));
+console.log("Effect: " + spread(sumNumbers, exampleArray_2));
--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function say(first, second) {
     console.log(`${first} ${second}`);
 
 }
+
 say(first_word, second_word);
 
 const show = (first, second) => console.log(first + " " + second);
@@ -32,7 +33,7 @@ const numbers = [5, 6, 7, 8, 9, 20, 56, 23, 98];
 average(1);
 average(1, 3);
 average(1, 3, 6, 6);
-average(1,2,3,4,5);
+average(1, 2, 3, 4, 5);
 average(...numbers);
 
 console.log("Task_4 -------------------:");
@@ -41,9 +42,95 @@ const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 average(...grades);
 
 console.log("Task_5 -------------------:");
-
 const data = [1, 4, 'Iwona', false, 'Nowak'];
 
-const [ , , firstname, , lastname] = data;
+const [, , firstname, , lastname] = data;
 
 console.log(`Name: ${firstname} ${lastname}`);
+
+console.log("Additional Task_1 --------:");
+
+function songDecoder(song) {
+    const djText = "WUB";
+    let textQuantity = "";
+    let count = 0;
+    let result = "";
+
+    for (let j = 0; j < song.length; j++) {
+
+        while (count == 0) {
+            count = Math.floor(Math.random() * 10);
+        }
+
+        for (let i = 0; i < count; i++) {
+            textQuantity += djText;
+        }
+        song = song.replace(" ", textQuantity);
+    }
+
+    count = Math.floor(Math.random() * 10);
+    textQuantity = "";
+
+    for (let i = 0; i < count; i++) {
+        textQuantity += djText;
+    }
+
+    result = `${textQuantity}${song}${textQuantity}`;
+
+    return result;
+}
+
+console.log(songDecoder("--wow-- --more-- --next--"));
+
+console.log("Additional Task_2 --------:");
+
+function shuffleIt(arr, ...arrayTwoPosition) {
+
+    let result = arr;
+    arrayTwoPosition.forEach(array => {
+        let first = arr[array[1]];
+        let second = arr[array[0]];
+        result[array[0]] = first;
+        result[array[1]] = second;
+    });
+
+    return result;
+}
+
+const arrayIn = [1, 2, 3, 4, 5];
+const arrayChange_1 = [0, 1];
+const arrayChange_2 = [1, 2];
+const arrayChange_3 = [2, 3];
+let result = shuffleIt(arrayIn, arrayChange_1, arrayChange_2, arrayChange_3);
+console.log(`Array before change --> [${arrayIn}], and after change --> [${result}]`);
+
+console.log("Additional Task_3 --------:");
+
+const arrayNumbers_1 = [65, 44, 12, 4];
+const arrayNumbers_2 = [136, 82, 72, 15];
+
+function sum(array) {
+    return array.reduce((total, num) => total + num);
+}
+
+console.log(`Sum numbers in array: [${arrayNumbers_1}] equals: ${sum(arrayNumbers_1)}`);
+console.log(`Sum numbers in array: [${arrayNumbers_2}] equals: ${sum(arrayNumbers_2)}`);
+
+console.log("Additional Task_4 --------:");
+
+function sumNumbers(newArray) {
+    let result = 0;
+    newArray.forEach(element => {
+        result += element;
+    });
+    return result
+}
+
+function spread(func, args) {
+    return func(args);
+}
+
+const exampleArray_1 = [1, 2, 3, 4, 5, 6, 7];
+const exampleArray_2 = [6, 7, 8, 9, 2, 5, 8];
+console.log(`Effect: ${spread(sumNumbers, exampleArray_1)}`);
+console.log(`Effect: ${spread(sumNumbers, exampleArray_2)}`);
